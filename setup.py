aca0bb285aed41e5c349f802c3eb1898e89493ea
mblance <matt@mblance.com>
2017-11-12 23:13:42 +0000

    Installer fix

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
+from sqlexport_tools import __version__
 
 description = 'SQL Export Tools with a Django Admin Mixin.  Exports CSV and XLSX.'
 
 setup(name='sqlexport-tools',
       packages=['sqlexport_tools'],
-      version='0.0.1',
+      version=__version__,
       description=description,
       author='Matt Buck',
       author_email='matt@mblance.com',
